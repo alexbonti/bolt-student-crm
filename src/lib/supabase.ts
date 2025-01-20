d74cc7ba9ae58959cb07a598008d03a7d09dd9ab
ChoudharyNavit22 <navitchoudhary22@gmail.com>
2025-01-20 11:49:32 +1100

    added ui fixes to the user management section

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,4 +4,3 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL||'//ltwdzsxpbythemeogbtm.s
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY||'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0d2R6c3hwYnl0aGVtZW9nYnRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY3MzgyMjEsImV4cCI6MjA1MjMxNDIyMX0.7yKDzBV7PFIwnsgcahDhQEkWI9KbXUTgfeelwu_Tu_w';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
